b4cd7195af17d5973db5bfab0a0a467fe9dabe4d
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-17 16:46:53 -0300

    Improve the Ansi class

--- src/unsi/ansi.py
+++ src/unsi/ansi.py
@@ -1,4 +1,4 @@
-"""A string that is aware of its own embedded ANSI escape sequences."""
+"""A string that can be disassembled into text and ANSI escape sequences."""
 
 from __future__ import annotations
 
@@ -13,7 +13,17 @@ PATTERN = re.compile(r"(\N{ESC}\[[\d;]*[a-zA-Z])")
 
 
 class Ansi(Text):
-    """A string that is aware of its own embedded ANSI escape sequences."""
+    r"""
+    A string that can be disassembled into text and ANSI escape sequences.
+
+    Examples
+    --------
+    >>> s = Ansi("\x1b[1;31mHello\x1b[m, world!")
+    >>> list(s.escapes())
+    [Escape('\x1b[1;31m'), 'Hello', Escape('\x1b[m'), ', world!']
+    >>> list(s.escapables())
+    [<Attribute.BOLD: 1>, Fore(color=Ansi256(1)), 'Hello', <Attribute.NORMAL: 0>, ', world!']
+    """
 
     def __repr__(self) -> Text:
         """Return a string representation of the object."""
@@ -27,10 +37,11 @@ class Ansi(Text):
             else:
                 yield Escape(match)
 
+    # TODO: this method and the Escapable type have ugly names
     def escapables(self) -> Iterable[Escapable | Text]:
         """Yield escapables and text in the order they appear."""
-        for chunk in self.escapes():
-            if not isinstance(chunk, Escape):
-                yield chunk
+        for escape in self.escapes():
+            if not isinstance(escape, Escape):
+                yield escape
             else:
-                yield from chunk.escapables()
+                yield from escape.escapables()
