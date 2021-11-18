07dfd2764bdd9cc872f6ea77c2b5540b776df6c4
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-18 13:53:58 -0300

    Test 8bit colors

--- tests/test_escape.py
+++ tests/test_escape.py
@@ -28,9 +28,29 @@ def test_vt100_escapes():
     assert _instr("\033[7m") == [SetAttribute(Attribute.REVERSE)]
 
 
-def test_ecma48_colors():
+def test_ecma48_8bit_colors():
     """Ensure the ECMA-48 colors are supported."""
 
-    assert _instr("\x1B[30m") == [
-        SetColor(role=ColorRole.FOREGROUND, color=ochre.Ansi256(0))
-    ]
+    def _fore(color):
+        return SetColor(ColorRole.FOREGROUND, color)
+
+    def _back(color):
+        return SetColor(ColorRole.BACKGROUND, color)
+
+    assert _instr("\x1B[30m") == [_fore(ochre.Ansi256(0))]
+    assert _instr("\x1B[31m") == [_fore(ochre.Ansi256(1))]
+    assert _instr("\x1B[32m") == [_fore(ochre.Ansi256(2))]
+    assert _instr("\x1B[33m") == [_fore(ochre.Ansi256(3))]
+    assert _instr("\x1B[34m") == [_fore(ochre.Ansi256(4))]
+    assert _instr("\x1B[35m") == [_fore(ochre.Ansi256(5))]
+    assert _instr("\x1B[36m") == [_fore(ochre.Ansi256(6))]
+    assert _instr("\x1B[37m") == [_fore(ochre.Ansi256(7))]
+
+    assert _instr("\x1B[40m") == [_back(ochre.Ansi256(0))]
+    assert _instr("\x1B[41m") == [_back(ochre.Ansi256(1))]
+    assert _instr("\x1B[42m") == [_back(ochre.Ansi256(2))]
+    assert _instr("\x1B[43m") == [_back(ochre.Ansi256(3))]
+    assert _instr("\x1B[44m") == [_back(ochre.Ansi256(4))]
+    assert _instr("\x1B[45m") == [_back(ochre.Ansi256(5))]
+    assert _instr("\x1B[46m") == [_back(ochre.Ansi256(6))]
+    assert _instr("\x1B[47m") == [_back(ochre.Ansi256(7))]
