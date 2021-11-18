410d6fa5380822ca2d59703b420faffbcbaa6565
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-18 13:12:35 -0300

    Test VT100 escapes

--- tests/test_escape.py
+++ tests/test_escape.py
@@ -1,9 +1,24 @@
 """Tests for the Escape class."""
 
 
-from unsi import Escape
+from unsi import Escape, SetAttribute
+from unsi.attribute import Attribute
 
 
 def test_escape_has_separator():
     """Ensure the class has a (constant) separator property."""
     assert hasattr(Escape, "SEPARATOR")
+
+
+def test_vt100_escapes():
+    """Ensure the classical VT100 escapes are supported."""
+
+    def instr(t):
+        return list(Escape(t).instructions())
+
+    assert instr("\x1B[0m") == instr("\033[m")
+    assert instr("\033[0m") == [SetAttribute(Attribute.NORMAL)]
+    assert instr("\033[1m") == [SetAttribute(Attribute.BOLD)]
+    assert instr("\033[4m") == [SetAttribute(Attribute.UNDERLINE)]
+    assert instr("\033[5m") == [SetAttribute(Attribute.BLINK)]
+    assert instr("\033[7m") == [SetAttribute(Attribute.REVERSE)]
